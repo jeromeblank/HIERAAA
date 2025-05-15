15e03673c025f79ffc74e57d6a183e3841998be7
Raiven-os <mrrruetas@tip.edu.ph>
2025-05-16 00:47:27 +0800

    Update settings.py

--- resumebuilder/settings.py
+++ resumebuilder/settings.py
@@ -155,6 +155,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # used for collectstatic in production
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
