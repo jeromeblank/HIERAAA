84e0891a0897ac65d74fcdd1ba494fa812798c2c
JeromeBlank <63527891+jeromeblank@users.noreply.github.com>
2025-05-16 11:57:53 +0800

    Update urls.py

--- resume/urls.py
+++ resume/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # Allauth login
     path('resume-builder/', views.resume_builder, name='resume_builder'),
     path('download_pdf/', views.download_pdf, name='download_pdf'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('job-search/', views.job_search, name='job_search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
